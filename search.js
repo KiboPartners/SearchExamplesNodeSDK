c5eb946fd0991468cf620a6e25e7a537cbc3316f
Alex Olivares <alex.olivares@kibocommerce.com>
2022-09-22 11:20:44 -0500

    formatting

--- search.js
+++ search.js
@@ -1,21 +1,18 @@
-require('dotenv').config()
+require("dotenv").config();
 
-
-// Create context for API calls 
+// Create context for API calls
 const apiContext = require("mozu-node-sdk/clients/platform/application")({
   context: {
     appKey: process.env.appKey,
     sharedSecret: process.env.sharedSecret,
     tenant: process.env.tenant,
     site: process.env.site,
-    baseUrl: "https://home.mozu.com"
-  }
-})
+    baseUrl: "https://home.mozu.com",
+  },
+});
 
-
-const Client = require('mozu-node-sdk/client');
+const Client = require("mozu-node-sdk/client");
 const constants = Client.constants;
-
 
 const searchFactory = Client.sub({
   suggest2: Client.method({
@@ -33,45 +30,42 @@ const searchFactory = Client.sub({
   }),
 });
 
-
 const searchResource = searchFactory(apiContext);
 
-
 // Used to suggest products and categories as the user types in the search box
-async function getSearchSuggestions(query = "", groups = "products,categories"){
-	try {
-
-		const suggestions = await searchResource.suggest2({ query, groups});
+async function getSearchSuggestions(query = "", groups = "products,categories") {
+  try {
+    const suggestions = await searchResource.suggest2({ query, groups });
 
     console.log(JSON.stringify(suggestions, null, 2));
-
-	} catch (error) {
-		console.log(error);	
-	}
-};
-
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 // Used for getting products to display on the serach results page
-async function siteSearch(query = "", fieldList = "score,productName,productImageUrls,priceL"){
-	try {
-
-		const searchResults = await searchResource.siteSearch({ query, fieldList, omitNamespace: true });
+async function siteSearch(query = "", fieldList = "score,productName,productImageUrls,priceL") {
+  try {
+    const searchResults = await searchResource.siteSearch({
+      query,
+      fieldList,
+      omitNamespace: true,
+    });
 
     console.log(JSON.stringify(searchResults, null, 2));
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 
-	} catch (error) {
-		console.log(error.message);	
-	}
-};
-
-(async function search(){
+(async function search() {
   try {
-		console.log("Results for type ahead suggestions:");
+    console.log("Results for type ahead suggestions:");
     await getSearchSuggestions("microwave");
 
-		console.log("Site search results:");
+    console.log("Site search results:");
     await siteSearch("kia");
   } catch (error) {
-    console.log(error); 
+    console.log(error);
   }
 })();
